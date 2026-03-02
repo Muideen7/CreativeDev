60c548bd156016a50ff818dd44e6ea9aad718691
Olayeye Muideen Ayomide <olayeyeayomide2000@gmail.com>
2026-03-02 15:38:03 +0100

    Update opengraph-image.tsx

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,6 +1,7 @@
 import { ImageResponse } from 'next/og';
 
-export const runtime = 'edge';
+// export const runtime = 'edge'; 
+
 export const alt = 'CreativeDev | Engineering Digital Emotions';
 export const size = { width: 1200, height: 630 };
 export const contentType = 'image/png';
